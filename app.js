a03175c29f9e19b56771d6c860f922fa62149a35
Gustav Syrén <g.e.syren@gmail.com>
2024-01-16 14:24:12 +0100

    object update

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 "use strict";
 const animals = ['apa', 'orangutang', 'gorilla', 'get', 'giraffe'];
 console.log(animals);
+const testObject = {
+    string: 'Hello',
+    number: 1,
+    bool: true,
+};
+console.log(testObject);
--- app.ts
+++ app.ts
@@ -1,3 +1,12 @@
 const animals: string[] = ['apa', 'orangutang', 'gorilla', 'get', 'giraffe']
 
 console.log(animals)
+
+
+const testObject = {
+    string: 'Hello',
+    number: 1,
+    bool: true,
+}
+
+console.log(testObject)
